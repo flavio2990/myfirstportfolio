000766e5f58cb73f5fd56e4a355cbfaf330ac2bb
Flavio Tosi <tosi.flaviol@gmail.com>
2023-07-11 11:23:34 -0300

    change some styles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Categories from './Categories';
 import Footer from './Footer';
 import items from './data';
 import AboutMe from './AboutMe';
-import { IonIcon } from '@ionic/react';
-import { mailOutline, logoGithub, documentTextOutline, logoLinkedin } from 'ionicons/icons';
-import FlavioLogo from './images/FlavioLogo.jpeg';
+import Header from './Header';
+import Title from './Title';
 
 
 const allCategories = ['all', ...new Set(items.map((item) => item.category))];
@@ -24,48 +23,27 @@ function App() {
     const newItems = items.filter((item) => item.category
       === category)
     setMenuItems(newItems)
-      }
-      return <main>
-        <section className='menu section'>
-          <div className='title'>
-            <img src={FlavioLogo} alt='FlavioLogo' className='Flavio' />
-            <div className="icon-container">
-            <IonIcon
-              icon={mailOutline}
-              size="large"
-              onClick={() => window.open("mailto:flaviotosi.leonel@gmail.com")}
-              className="icon"
-            />
-            <IonIcon
-              icon={logoGithub}
-              size="large"
-              onClick={() => window.open("https://github.com/flavio2990")}
-              className="icon"
-            />
-            <a href={require('../src/images/FlavioCV.pdf')} target="_blank" rel="noopener noreferrer" className="no-highlight">
-              <IonIcon icon={documentTextOutline} size="large" className="icon" />
-            </a>
-            <IonIcon
-              icon={logoLinkedin}
-              size='large'
-              onClick={() => window.open('https://www.linkedin.com/in/flavio-tosi-921286266/')}
-              className='icon'
-            />
-              </div>
-            <h1>Welcome!</h1>
-            <p>My name is Flavio Tosi, I am a front-end Junior Developer based in Entre Rios, Argentina.<br /> I am self-taught, with a solid foundation in JavaScript, React and React Native.<br /> Competence acquired through intensive self-study, practical experience and courses, all validated by Meta certificates.<br /> I strive for practical development and professional growth in a collaborative environment.</p>
-            <div className='underline'></div>
-          </div>
-          <Categories categories={categories}
-            filterItems={filterItems}
-          />
-          <Menu items={menuItems}
-          />
-          <div>
-            <AboutMe/>
-          <Footer/>
-          </div>
-        </section>
-      </main>;
-    }
-  export default App;
+  }
+  return <main>
+    <section className='menu section'>
+      <div className='title'>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Title />
+        </div>
+      </div>
+      <Categories categories={categories}
+        filterItems={filterItems}
+      />
+      <Menu items={menuItems}
+      />
+      <div>
+        <AboutMe />
+        <Footer />
+      </div>
+    </section>
+  </main>;
+}
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { IonIcon } from '@ionic/react';
+import { mailOutline, logoGithub, documentTextOutline, logoLinkedin } from 'ionicons/icons';
+import iconflaviodev from './images/iconflaviodev.jpeg';
+
+
+export default function Header() {
+    return (
+        <div className='title'>
+            <img src={iconflaviodev} alt='FlavioLogo' className='Flavio' />
+            <div className="icon-container">
+                <IonIcon
+                    icon={mailOutline}
+                    size="large"
+                    onClick={() => window.open("mailto:flaviotosi.leonel@gmail.com")}
+                    className="icon"
+                />
+                <IonIcon
+                    icon={logoGithub}
+                    size="large"
+                    onClick={() => window.open("https://github.com/flavio2990")}
+                    className="icon"
+                />
+                <a href={require('../src/images/FlavioCV.pdf')} target="_blank" rel="noopener noreferrer" className="no-highlight">
+                    <IonIcon icon={documentTextOutline} size="large" className="icon" />
+                </a>
+                <IonIcon
+                    icon={logoLinkedin}
+                    size='large'
+                    onClick={() => window.open('https://www.linkedin.com/in/flavio-tosi-921286266/')}
+                    className='icon'
+                />
+            </div>
+
+        </div>
+    )
+}
